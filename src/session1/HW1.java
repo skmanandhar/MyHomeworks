e3bedab523f717bdcb74fb29faa420ac83644761
skmanandhar <skmanandhar@gmail.com>
2024-01-09 21:02:51 -0800

    hw1

---
+++ src/session1/HW1.java
@@ -0,0 +1,20 @@
+package session1;
+
+import java.util.Arrays;
+
+public class HW1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, my name is Shyam");
+
+//		int a[] = {1,3,54,2,33};
+		String[] b = {"ssd", "mkk","ier"};
+		Arrays.sort(b);
+		
+		for (String i:b) {
+			System.out.println(i);
+		}
+
+	}
+}
